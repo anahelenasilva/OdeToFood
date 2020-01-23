c9ccc06d87d87f32268e6fb5d9347df97c1c47af
Ana Helena <anahelenarp@hotmail.com>
2020-01-23 13:32:10 -0300

    Add project OdeToFood.Data

---
+++ OdeToFood/OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,24 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        public InMemoryRestaurantData()
+        {
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
