9bbd1852d14dd7690db8583754b04d82b0760319
Ana Helena <anahelenarp@hotmail.com>
2020-01-23 21:56:51 -0300

    Add NotFound page

--- OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
+++ OdeToFood/OdeToFood/Pages/Restaurants/Detail.cshtml.cs
@@ -5,16 +5,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using OdeToFood.Core;
+using OdeToFood.Data;
 
 namespace OdeToFood
 {
     public class DetailModel : PageModel
     {
         public Restaurant Restaurant { get; set; }
+        public IRestaurantData RestaurantData { get; }
 
-        public void OnGet(int id)
+        public DetailModel(IRestaurantData restaurantData)
         {
-            Restaurant = new Restaurant();
+            RestaurantData = restaurantData;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Restaurant = RestaurantData.GetById(id);
+
+            if (Restaurant == null)
+                return RedirectToPage("./NotFound");
+
+            return Page();
         }
     }
 }
