f6bf83e89d993beeb9d37a536372faf79a219ddc
Ana Helena <anahelenarp@hotmail.com>
2020-02-11 13:30:28 -0300

    add context

---
+++ OdeToFood/OdeToFood.Data/Context.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Restaurant> Restaurant { get; set; }
+    }
+}
